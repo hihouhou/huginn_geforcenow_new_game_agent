f06e255c3eb8fd86202f77b4d225d71617c9b965
Nicolas Germain <ngermain@hihouhou.com>
2022-06-24 23:41:31 +0000

    I don't know why an other curl is needed for checking all available games...

--- lib/huginn_geforcenow_new_game_agent/geforcenow_new_game_agent.rb
+++ lib/huginn_geforcenow_new_game_agent/geforcenow_new_game_agent.rb
@@ -75,6 +75,11 @@ module Agents
     private
 
     def check_status()
+      first
+      second
+    end
+
+    def first
       uri = URI.parse("https://static.nvidiagrid.net/supported-public-game-list/locales/gfnpc-en-US.json")
       request = Net::HTTP::Get.new(uri)
       request["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0"
@@ -143,6 +148,85 @@ module Agents
         create_event payload: payload
         if payload.to_s != memory['last_status']
           memory['last_status'] = payload.to_s
+        end
+      end
+    end
+
+    def second
+      uri = URI.parse("https://api-prod.nvidia.com/gfngames/v1/gameList")
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/json;charset=utf-8"
+      request["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0"
+      request["Accept"] = "*/*"
+      request["Accept-Language"] = "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3"
+      request["Origin"] = "https://www.nvidia.com"
+      request["Connection"] = "keep-alive"
+      request["Referer"] = "https://www.nvidia.com/"
+      request["Sec-Fetch-Dest"] = "empty"
+      request["Sec-Fetch-Mode"] = "cors"
+      request["Sec-Fetch-Site"] = "same-site"
+      request["Dnt"] = "1"
+      request["Pragma"] = "no-cache"
+      request["Cache-Control"] = "no-cache"
+      request["Te"] = "trailers"
+      request.body = "{ apps(country:\"FR\" language:\"fr_FR\" after:\"MTMwMA==\" ) {\n  numberReturned\n  pageInfo {\n    endCursor\n    hasNextPage\n  }\n  items {\n  title\n  sortName\nvariants{\n  appStore\n  publisherName\n  \n    }\n  }\n}}"
+      
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      if interpolated['debug'] == 'true'
+        log "response.body"
+        log response.body
+      end
+
+      log "request status : #{response.code}"
+      payload = JSON.parse(response.body)
+      if interpolated['changes_only'] == 'true'
+        if payload.to_s != memory['last_status_second']
+          if "#{memory['last_status_second']}" == ''
+            payload['data']['apps']['items'].each do |game|
+              create_event payload: game
+            end
+          else
+            last_status = memory['last_status_second'].gsub("=>", ": ").gsub(": nil,", ": null,")
+            last_status = JSON.parse(last_status)
+            payload['data']['apps']['items'].each do |game|
+              found = false
+              if interpolated['debug'] == 'true'
+                log "game"
+                log game
+              end
+              last_status['data']['apps']['items'].each do |gamebis|
+                if game == gamebis
+                  found = true
+                end
+                if interpolated['debug'] == 'true'
+#                  log "gamebis"
+#                  log gamebis
+                  log "found is #{found}!"
+                end
+              end
+#              if found == false && game['status'] == 'AVAILABLE'
+              if found == false
+                if interpolated['debug'] == 'true'
+                  log "found is #{found}! so event created"
+                  log game
+                end
+                create_event payload: game
+              end
+            end
+          end
+          memory['last_status_second'] = payload.to_s
+        end
+      else
+        create_event payload: payload
+        if payload.to_s != memory['last_status_second']
+          memory['last_status_second'] = payload.to_s
         end
       end
     end
